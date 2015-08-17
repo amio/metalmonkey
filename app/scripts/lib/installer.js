962345d2610f65dd213fb90d604dce7dd5635386
Amio <amio.cn@gmail.com>
2015-08-17 23:04:36 +0800

    (snip) installerAgent use blocking method

--- app/scripts/lib/installer.js
+++ app/scripts/lib/installer.js
@@ -2,37 +2,39 @@ const usp = require('userscript-parser')
 const reg = require('./registry')
 
 export function installer () {
-  const installerFilter = {
-    urls: ['*://*/*.js']
+  //
+  chrome.webRequest.onBeforeRequest.addListener(
+    installerAgent,
+    { urls: ['*://*/*.user.js'] },
+    [ 'blocking' ]
+  )
+
+}
+
+function installerAgent (details) {
+  // Only deal with userscripts request from webpage
+  const isUserJS = /\.user\.js$/.test(details.url)
+  const isFromExtension = details.tabId === -1
+
+  console.log(isUserJS, isFromExtension, details)
+
+  if (!isUserJS || isFromExtension) return
+
+  setTimeout(function () {
+    fetch(details.url)
+      .then(function (response) {
+        return response.text()
+      })
+      .then(function (text) {
+        let newItem = {}
+        newItem[details.url] = text
+        chrome.storage.local.set(newItem)
+        reg.add(details.url, text)
+      })
+
+  }, 4000)
+
+  return {
+    redirectUrl: 'javascript:history.back()'
   }
-  function installerAgent (details) {
-    // Only deal with userscripts request from webpage
-    const isUserJS = /\.user\.js$/.test(details.url)
-    const isFromExtension = details.tabId === -1
-    if (!isUserJS || isFromExtension) return
-
-    setTimeout(function () {
-      fetch(details.url)
-        .then(function (response) {
-          return response.text()
-        })
-        .then(function (text) {
-          let newItem = {}
-          newItem[details.url] = text
-          chrome.storage.local.set(newItem)
-
-          mmRegistry.add(details.url, text)
-        })
-
-    }, 2000)
-
-    console.log(isUserJS, isFromExtension, details)
-
-    return {
-      cancel: true
-    }
-  }
-
-  chrome.webRequest.onBeforeRequest.addListener(installerAgent, installerFilter)
-
 }
