d3999b202e91683be9d5fe508588bb905fb23973
Amio <amio.cn@gmail.com>
2015-08-17 23:42:32 +0800

    (snip) add dev-helper.js

---
+++ app/scripts/lib/dev-helper.js
@@ -0,0 +1,10 @@
+export {
+  env,
+  log
+}
+
+const env = 'dev'
+
+function log(){
+  console.log.apply(console, [].slice.call(arguments))
+}
--- app/scripts/lib/installer.js
+++ app/scripts/lib/installer.js
@@ -1,5 +1,6 @@
 const usp = require('userscript-parser')
 const reg = require('./registry')
+import * as dev from './dev-helper.js'
 
 export function installer () {
   //
@@ -15,24 +16,20 @@ function installerAgent (details) {
   // Only deal with userscripts request from webpage
   const isUserJS = /\.user\.js$/.test(details.url)
   const isFromExtension = details.tabId === -1
-
-  console.log(isUserJS, isFromExtension, details)
+  dev.log(isUserJS, isFromExtension, details)
 
   if (!isUserJS || isFromExtension) return
 
-  setTimeout(function () {
-    fetch(details.url)
-      .then(function (response) {
-        return response.text()
-      })
-      .then(function (text) {
-        let newItem = {}
-        newItem[details.url] = text
-        chrome.storage.local.set(newItem)
-        reg.add(details.url, text)
-      })
-
-  }, 4000)
+  fetch(details.url)
+    .then(function (response) {
+      return response.text()
+    })
+    .then(function (text) {
+      let newItem = {}
+      newItem[details.url] = text
+      chrome.storage.local.set(newItem)
+      reg.add(details.url, text)
+    })
 
   return {
     redirectUrl: 'javascript:history.back()'
