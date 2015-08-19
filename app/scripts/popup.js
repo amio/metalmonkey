3385a6d68552c10d574401afa6e037d014e04019
Amio <amio.cn@gmail.com>
2015-08-20 00:16:51 +0800

    (snip) add manage.html

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,1 +1,14 @@
+const optionBtn = document.querySelector('#menu-options')
 
+optionBtn.addEventListener('click', function (ev) {
+  // chrome.tabs.create({ 'url': 'chrome://extensions/?options=' + chrome.runtime.id })
+  var optionsUrl = chrome.extension.getURL('html/options.html')
+
+  chrome.tabs.query({url: optionsUrl}, function (tabs) {
+    if (tabs.length) {
+      chrome.tabs.update(tabs[0].id, {active: true})
+    } else {
+      chrome.tabs.create({url: optionsUrl})
+    }
+  })
+})
--- tasks/styles.js
+++ tasks/styles.js
@@ -1,43 +1,43 @@
-var gulp = require('gulp');
-var less = require('gulp-less');
-var watch = require('gulp-watch');
-var sourcemaps = require('gulp-sourcemaps');
-var LessPluginCleanCSS = require("less-plugin-clean-css");
-var LessPluginAutoPrefix = require('less-plugin-autoprefix');
-var livereload = require('gulp-livereload');
+var gulp = require('gulp')
+var less = require('gulp-less')
+var watch = require('gulp-watch')
+var sourcemaps = require('gulp-sourcemaps')
+var LessPluginCleanCSS = require('less-plugin-clean-css')
+var LessPluginAutoPrefix = require('less-plugin-autoprefix')
+var livereload = require('gulp-livereload')
 
 /***********************************************************
  * Configue
  ***********************************************************/
 var src = [
   'app/styles/*.less',
-];
-var dest = 'dist/styles';
+]
+var dest = 'dist/styles'
 
 var cleancss = new LessPluginCleanCSS({
   advanced: true
-});
+})
 
 var autoprefix = new LessPluginAutoPrefix({
-  browsers: ["last 2 versions"]
-});
+  browsers: ['last 2 versions']
+})
 
 /***********************************************************
  * Build
  ***********************************************************/
-gulp.task('styles', function() {
+gulp.task('styles', function () {
   return gulp.src(src)
     .pipe(less({
       plugins: [autoprefix, cleancss]
     }))
-    .pipe(gulp.dest(dest));
-});
+    .pipe(gulp.dest(dest))
+})
 
 /***********************************************************
  * Watch
  ***********************************************************/
-gulp.task('styles:dev', function() {
-  livereload.listen();
+gulp.task('styles:dev', function () {
+  livereload.listen()
   return gulp.src(src)
     .pipe(watch(src))
     // .pipe(sourcemaps.init())
@@ -47,5 +47,5 @@ gulp.task('styles:dev', function() {
     // .pipe(sourcemaps.write())
     .pipe(gulp.dest(dest))
     // Livereload
-    .pipe(livereload());
-});
+    .pipe(livereload())
+})
