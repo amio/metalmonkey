e79512125f77aefdad4cd0e189d7a11fe71f80df
Amio <amio.cn@gmail.com>
2015-11-10 22:24:56 +0800

    (chore) update deps.

--- tasks/images.js
+++ tasks/images.js
@@ -1,27 +1,27 @@
-var gulp = require('gulp');
-var imagemin = require('gulp-imagemin');
-var watch = require('gulp-watch');
+var gulp = require('gulp')
+var imagemin = require('gulp-imagemin')
+var watch = require('gulp-watch')
 
 /***********************************************************
  * Configue
  ***********************************************************/
-var src = 'app/images/**/*';
-var dest = 'dist/images';
+var src = 'app/images/**/*'
+var dest = 'dist/images'
 
 /***********************************************************
  * Build
  ***********************************************************/
-gulp.task('images', function() {
+gulp.task('images', function () {
   return gulp.src(src)
     .pipe(imagemin())
-    .pipe(gulp.dest(dest));
-});
+    .pipe(gulp.dest(dest))
+})
 
 /***********************************************************
  * Watch
  ***********************************************************/
-gulp.task('images:dev', function() {
+gulp.task('images:dev', function () {
   return gulp.src(src)
     .pipe(watch(src))
-    .pipe(gulp.dest(dest));
-});
+    .pipe(gulp.dest(dest))
+})
