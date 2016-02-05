570635d024dfa46b3d7c2be2beb970e0a108659c
Amio <amio.cn@gmail.com>
2016-02-06 00:43:33 +0800

    (chore) rename install(sidebar) to discover

--- app/scripts/comp/sidebar.jsx
+++ app/scripts/comp/sidebar.jsx
@@ -23,7 +23,7 @@ const Sidebar = React.createClass({
         <ul className="navs">
           <li className={ navClasses('manage') }><a href="#/manage">manage scripts</a></li>
           <li className={ navClasses('editor') }><a href="#/editor">create • edit</a></li>
-          <li className={ navClasses('install') }><a href="#/install">install</a></li>
+          <li className={ navClasses('install') }><a href="#/install">discover</a></li>
           <li className={ navClasses('options') }><a href="#/options">options</a></li>
         </ul>
         <div className="etc">
