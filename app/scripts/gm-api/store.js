f81d47d3ceb387c551b031a8f99e2c39e28556c0
Amio <amio.cn@gmail.com>
2016-02-10 17:29:02 +0800

    Add gm-api:store #1

---
+++ app/scripts/gm-api/store.js
@@ -0,0 +1,49 @@
+const STORAGE_PREFIX = 'SCRIPT:'
+
+class ScriptStorage {
+  constructor (scriptId) {
+    this.storage = {}
+    this.storageId = STORAGE_PREFIX + scriptId
+    this.syncFromChromeStorage()
+  }
+
+  setValue (name, value) {
+    this.storage[name] = value
+    this.syncToChromeStorage()
+  }
+
+  getValue (name, defaultValue) {
+    return this.storage[name] === undefined ? defaultValue : this.storage[name]
+  }
+
+  listValue () {
+    return Object.keys(this.storage)
+  }
+
+  deleteValue (name) {
+    this.storage[name] = undefined
+    this.syncToChromeStorage()
+  }
+
+  syncToChromeStorage () {
+    chrome.storage.local.set({
+      [this.storageId]: JSON.stringify(this.storage)
+    }, function () {
+      chrome.runtime.lastError && console.error('Sync to ChromeStorage failed.')
+    })
+  }
+
+  syncFromChromeStorage (callback) {
+    chrome.storage.local.get(this.storageId, items => {
+      if (chrome.runtime.lastError) {
+        console.error('Sync from ChromeStorage failed.')
+        return
+      }
+      this.storage = JSON.parse(items[this.storageId])
+    })
+  }
+}
+
+export default function initScriptStorage (scriptId) {
+  return new ScriptStorage(scriptId)
+}
