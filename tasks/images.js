90bb2469cea32ca1b068ca89255da4138342c097
Amio <amio.cn@gmail.com>
2016-07-10 20:16:38 +0800

    Disable imagemin in images task.

--- tasks/images.js
+++ tasks/images.js
@@ -1,25 +1,25 @@
 var gulp = require('gulp')
-var imagemin = require('gulp-imagemin')
+// var imagemin = require('gulp-imagemin')
 var watch = require('gulp-watch')
 
-/***********************************************************
+/*
  * Configue
- ***********************************************************/
-var src = 'app/images/**/*'
+ */
+var src = 'app/images/*'
 var dest = 'dist/images'
 
-/***********************************************************
+/*
  * Build
- ***********************************************************/
+ */
 gulp.task('images', function () {
   return gulp.src(src)
-    .pipe(imagemin())
+    // .pipe(imagemin())
     .pipe(gulp.dest(dest))
 })
 
-/***********************************************************
+/*
  * Watch
- ***********************************************************/
+ */
 gulp.task('images:dev', function () {
   return gulp.src(src)
     .pipe(watch(src))
