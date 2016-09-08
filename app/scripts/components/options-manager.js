6827c8ba72e56a5ca94d4d59196b525b619c6a6e
Amio <amio.cn@gmail.com>
2016-09-09 00:04:09 +0800

    Set option page size for small screen.

--- app/scripts/components/options-manager.js
+++ app/scripts/components/options-manager.js
@@ -5,8 +5,11 @@ import ScriptsList from './scripts-list.js'
 import theme from '../themes/default.js'
 
 export default () => (
-  <div>
-    <AppBar title='MetalMonkey / Manage Scripts' titleStyle={theme.appBar.titleStyle} />
+  <div style={styles.layout}>
+    <AppBar
+      title='MetalMonkey / Manage Scripts'
+      titleStyle={theme.appBar.titleStyle}
+      showMenuIconButton={false} />
     <Paper style={styles.contentWrapper}>
       <ScriptsList />
     </Paper>
@@ -14,8 +17,15 @@ export default () => (
 )
 
 const styles = {
+  layout: {
+    display: 'flex',
+    flexDirection: 'column',
+    height: '100%'
+  },
   contentWrapper: {
     maxWidth: '1100px',
+    width: '80%',
+    overflow: 'scroll',
     margin: '3rem auto',
     fontSize: '16px'
   }
