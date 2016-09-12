6a58d9cf544eaf6d5c09ebe4e15c257780168e34
Amio <amio.cn@gmail.com>
2016-09-12 16:32:35 +0800

    List userscript in current tab. #12

--- app/scripts/Popup.js
+++ app/scripts/Popup.js
@@ -1,35 +1,73 @@
+import { getMatchedUserscripts } from './lib/registry.js'
 
-// Click listener for "Manage install scripts"
-document.getElementById('menu-manage')
-.addEventListener('click', e => openExtensionPage('options.html', '#manage'))
-
-// Set footer version
-const version = require('../../package.json').version
-document.querySelector('.version').innerText = 'v' + version
+// Click listeners
+const menuManage = document.getElementById('menu-manage')
+const menuCreate = document.getElementById('menu-create-script')
+menuManage.addEventListener('click', e => openExtensionPage('options.html', '#manage'))
+menuCreate.addEventListener('click', e => openExtensionPage('options.html', '#create'))
 
 // Menu items for current page
 const { React, ReactDOM } = window
+class PopupList extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      matched: []
+    }
+  }
 
-const PopupApp = props => {
-  return (
-    <ul className='menu'>
-      <li className='menu-item' id='menu-options'>{'Youtube screenshot button'}</li>
-    </ul>
-  )
+  componentDidMount () {
+    chrome.tabs.getSelected(null, tab => {
+      this.setState({
+        currentUrl: tab.url
+      })
+
+      getMatchedUserscripts(tab.url).then(uss => this.setState({
+        matched: uss
+      }))
+    })
+  }
+
+  goEdit () {
+    const uri = encodeURIComponent(this.usid)
+    openExtensionPage('options.html', '#edit/' + uri)
+  }
+
+  render () {
+    console.log(this.state.matched)
+    return this.state.matched.length ? (
+      <ul className='menu'>
+        {
+          this.state.matched.map(us => {
+            return (
+              <li key={us.usid}
+                className='menu-item'
+                onClick={this.goEdit.bind(us)}>
+                { us.name }
+              </li>
+            )
+          })
+        }
+      </ul>
+    ) : null
+  }
 }
-
-ReactDOM.render(<PopupApp />, document.getElementById('app'))
+ReactDOM.render(<PopupList />, document.getElementById('app'))
 
 // Open page
-function openExtensionPage (page, hashRoute) {
+function openExtensionPage (pageName, pageArgs) {
   // page: 'options.html' or 'options.html#create'
-  const pageURL = chrome.extension.getURL(page)
+  const pageURL = chrome.extension.getURL(pageName)
 
   chrome.tabs.query({url: pageURL}, function (tabs) {
     if (tabs.length) {
       chrome.tabs.update(tabs[0].id, {active: true})
     } else {
-      chrome.tabs.create({url: pageURL + (hashRoute || '')})
+      chrome.tabs.create({url: pageURL + (pageArgs || '')})
     }
   })
 }
+
+// Set footer version
+const version = require('../../package.json').version
+document.querySelector('.version').innerText = 'v' + version
