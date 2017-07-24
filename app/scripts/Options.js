b8f6cab838bc539757f33c89322d32988a7eb7e0
Amio <amio.cn@gmail.com>
2017-07-24 18:00:20 +0800

    Remove react-mdl.

--- app/scripts/Options.js
+++ app/scripts/Options.js
@@ -8,8 +8,6 @@ import Editor from './components/options-editor.js'
 import theme from './themes/default.js'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 
-import 'react-mdl/extra/material.js'
-
 const OptionsApp = () => (
   <MuiThemeProvider muiTheme={theme}>
     <ReactDocumentTitle title='METALMONKEY'>
