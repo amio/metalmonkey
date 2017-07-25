15ed89a76c7b74436cc06fc552c46bb70d821d4f
Amio <amio.cn@gmail.com>
2017-07-25 13:26:13 +0800

    Bypass html when blocking user.js requests.

--- app/scripts/lib/installerAgent.js
+++ app/scripts/lib/installerAgent.js
@@ -1,21 +1,37 @@
 export default function () {
-  // Blocking request for "*.user.js"
-  chrome.webRequest.onBeforeRequest.addListener(
-    userscriptAgent,
-    { urls: ['*://*/*.user.js'] },
-    ['blocking']
+  // Blocking requests for "*.user.js"
+  chrome.webRequest.onHeadersReceived.addListener(
+    userscriptRequestListener,
+    requestFilter,
+    extraInfoSpec
   )
+}
 
-  function userscriptAgent (details) {
-    // Only deal with userscripts request by 'main_frame'
-    // (bypass requests for <script />, etc.)
-    if (details.type !== 'main_frame') return
+const requestFilter = {
+  urls: ['https://*/*.user.js'],
+  types: ['main_frame']
+}
 
+const extraInfoSpec = ['blocking', 'responseHeaders']
+
+function userscriptRequestListener (details) {
+  if (isUserScript(details.responseHeaders)) {
     const installerPage = chrome.extension.getURL('installer.html')
     chrome.tabs.create({'url': `${installerPage}?script=${details.url}`})
-
-    return {
-      redirectUrl: 'javascript:history.back()'
-    }
+    return { redirectUrl: 'javascript:history.back()' }
+  } else {
+    return {}
   }
+}
+
+function isUserScript (headers) {
+  const metas = headers.reduce((meta, curr) => {
+    if (curr.name === 'Content-Type') {
+      meta.isHTML = /html/.test(curr.value)
+      meta.isScript = /javascript/.test(curr.value)
+    }
+    return meta
+  }, {})
+
+  return !metas.isHTML // todo: need more accurate detection
 }
