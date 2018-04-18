3612a7977da44619e25cd5df0d0f1530a07a06e1
Amio <amio.cn@gmail.com>
2018-04-18 15:07:05 +0800

    Remove popup.html, use main.html instead for toolbar icon click

--- app/background.js
+++ app/background.js
@@ -1,1 +1,8 @@
 import browser from 'webextension-polyfill'
+
+// Open main.html when click browser_action icon
+browser.browserAction.onClicked.addListener(tab => {
+  browser.tabs.create({
+    url: browser.extension.getURL('main.html')
+  })
+})
---
+++ app/components/main-index.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default () => {
+  console.log(1)
+  return (
+    <div>Main Index</div>
+  )
+}
