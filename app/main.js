d1fb759329e440a5d119e3236565094b76eb9b8e
Amio <amio.cn@gmail.com>
2018-04-18 15:07:05 +0800

    Remove popup.html, use main.html instead for toolbar icon click

---
+++ app/main.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import MainIndex from './components/main-index.jsx'
+
+ReactDOM.render(<MainIndex />, document.getElementById('app'))
--- app/options.js
+++
@@ -1,1 +0,0 @@
-console.log('MM: Options')
