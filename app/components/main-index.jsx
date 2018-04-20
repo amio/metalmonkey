86c45c5ebf159964e43e7d14604521a69431f8cd
Amio <amio.cn@gmail.com>
2018-04-20 18:26:11 +0800

    Add styled-jsx

--- app/components/main-index.jsx
+++ app/components/main-index.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import AppBar from 'material-ui/AppBar'
+import Toolbar from 'material-ui/Toolbar'
+import Typography from 'material-ui/Typography'
 import Button from 'material-ui/Button'
+import IconButton from 'material-ui/IconButton'
+import MenuIcon from '@material-ui/icons/Menu'
 
-export default () => {
+import Layout from './layout.jsx'
+
+function MainIndex (props) {
   return (
-    <div>Main Index<Button>as</Button></div>
+    <Layout>
+      <AppBar position='static'>
+        <div>
+          <Toolbar>
+            <IconButton color='inherit'>
+              <MenuIcon />
+            </IconButton>
+            <Typography variant='title' color='inherit' className='main-title'>
+              METALMONKEY
+            </Typography>
+            <Button color='inherit'>Login</Button>
+          </Toolbar>
+        </div>
+      </AppBar>
+      <style jsx>{`
+        div :global(.main-title) {
+          flex: 1
+        }
+      `}</style>
+    </Layout>
   )
 }
+
+export default MainIndex
