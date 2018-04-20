05947926bbd6f2c9b973a4f0983b19d0968a7478
Amio <amio.cn@gmail.com>
2018-04-21 00:21:25 +0800

    Add theme primary color

--- app/components/layout.jsx
+++ app/components/layout.jsx
@@ -1,13 +1,25 @@
-export default ({children, style, className = ''}) => (
-  <div className={'root ' + className} style={style}>
-    { children }
-    <style jsx>{`
-      flex-grow: 1
-    `}</style>
-    <style jsx global>{globalStyles}</style>
-  </div>
+import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles'
+import CssBaseline from 'material-ui/CssBaseline'
+
+export default ({children, style, className}) => (
+  <MuiThemeProvider theme={theme}>
+    <div id='root' className={className} style={style}>
+      { children }
+      <CssBaseline />
+      <style jsx global>{globalStyles}</style>
+    </div>
+  </MuiThemeProvider>
 )
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#D55'
+    }
+  }
+})
+
 const globalStyles = `
-  html, body { margin: 0 }
+  html, body { font-size: 16px; }
+  #root { font-family: Roboto, sans-serif; }
 `
