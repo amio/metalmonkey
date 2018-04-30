3f333adba7de825a373d3330a0291fa77a5efb15
Amio <amio.cn@gmail.com>
2018-04-30 11:16:31 +0800

    Add matchAssetsByURL()

--- app/components/manager-index.jsx
+++ app/components/manager-index.jsx
@@ -7,10 +7,7 @@ import { listAssets } from '../libs/store.js'
 export default class ManagerIndex extends React.Component {
   async componentDidMount () {
     const assets = await listAssets()
-    Object.keys(assets).map(key => {
-      console.log(assets[key])
-      return true
-    })
+    console.log(assets)
   }
 
   render () {
--- app/libs/store.js
+++ app/libs/store.js
@@ -1,4 +1,5 @@
 import browser from 'webextension-polyfill'
+import match from 'url-match-patterns'
 
 async function installAsset (url, meta, code) {
   const key = url
@@ -11,7 +12,16 @@ async function listAssets (keys) {
   return browser.storage.local.get(keys)
 }
 
+async function matchAssetsByURL (url) {
+  const entries = await listAssets().then(assets => Object.values(assets))
+  return entries.filter(entry => {
+    const patterns = entry.meta && entry.meta.matches && entry.meta.matches
+    return patterns && patterns.find(p => match(p, url))
+  })
+}
+
 export {
   installAsset,
-  listAssets
+  listAssets,
+  matchAssetsByURL
 }
