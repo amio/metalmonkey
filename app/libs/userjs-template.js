d925658d8ac7c182e8064fa61e3320c2535c3a32
Amio <amio.cn@gmail.com>
2018-05-06 10:43:41 +0800

    Add userjs-template

---
+++ app/libs/userjs-template.js
@@ -0,0 +1,12 @@
+export default `
+exports.meta = {
+  name: 'A Userscript',
+  description: 'Do awesome things.',
+  version: '1.0.0',
+  matches: ['https://youtube.com']
+}
+
+module.exports = function (env, getOptions, setOptions) {
+  // main contents
+}
+`
