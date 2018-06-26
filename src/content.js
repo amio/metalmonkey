bc91e003a934fca3a3abbce9d98833228052f053
Amio <amio.cn@gmail.com>
2018-06-26 13:01:33 +0800

    Remove install button on npmjs.com pages

--- src/content.js
+++ src/content.js
@@ -1,12 +1,5 @@
 import browser from 'webextension-polyfill'
 import parseMeta from './libs/parse-meta.js'
-import match from 'url-match-patterns'
-import * as userjsInstall from './libs/install-button.user.js'
-
-// Userscript Install Button for npmjs.com/package/*
-if (userjsInstall.options.matches.find(p => match(p, window.location.href))) {
-  userjsInstall.default()
-}
 
 // Inject install button on `*.user.mjs` pages
 if (document.title === '' && /\.user\.mjs/.test(window.location.href)) {
